61b8478a92c0b8581bd31ca9347c8b9ad3d51222
Faten Salmi <faten.salmi@gmail.com>
2019-11-21 17:36:48 +0100

    Mister Cocktail back

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails/:id', to: 'cocktails#show'
+  # get 'cocktails/new', to: 'cocktails#new'
+  # post 'cocktails', to: 'cocktails#create'
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:index, :new, :create]
+    # resources :ingredients, only: [:index, :new, :create]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "orange jiuce")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "soho")
+Ingredient.create(name: "ananas juice")
+Ingredient.create(name: "water")
